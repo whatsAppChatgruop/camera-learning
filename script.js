529b0a2eb14ac27af9e51d991edc71bcb8206e4d
whatsAppChatgruop <shilasanam@gmail.com>
2026-07-17 15:59:56 +0430

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
+let video = document.getElementById("video");
+
 function openCamera() {
 
     navigator.mediaDevices.getUserMedia({
         video: true
     })
-
     .then(function(stream) {
-
-        const video = document.getElementById("video");
         video.srcObject = stream;
-
     })
-
     .catch(function(error) {
-
-        alert("دسترسی به دوربین داده نشد");
-
+        alert("دوربین فعال نشد");
         console.log(error);
-
     });
 
 }
+
+
 function takePhoto() {
 
-    const video = document.getElementById("video");
-    const canvas = document.getElementById("canvas");
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext("2d");
 
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
 
-    const context = canvas.getContext("2d");
-
     context.drawImage(video, 0, 0);
+
+    alert("عکس گرفته شد");
 
 }
